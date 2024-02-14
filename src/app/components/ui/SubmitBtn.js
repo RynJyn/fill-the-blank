a2f2b9e93cc04fcffaf326c98f619dc5d2566fb5
Ryan Jeynes <ryanjeynes@outlook.com>
2024-02-14 18:02:40 +0000

    Add styling to SubmitBtn component

--- src/app/components/ui/SubmitBtn.js
+++ src/app/components/ui/SubmitBtn.js
@@ -1,8 +1,10 @@
 import React from "react";
 
+import "../../styles/ui/SubmitBtn.scss";
+
 function SubmitBtn(props)
 {
-    return <button onClick={props.onClick ? props.onClick : ()=>{console.log("Ouch!");}}type="button">{props.text ? props.text : "Submit"}</button>;
+    return <button className="submit-btn" onClick={props.onClick ? props.onClick : ()=>{console.log("Ouch!");}}type="button">{props.text ? props.text : "Submit"}</button>;
 }
 
 export default SubmitBtn;
